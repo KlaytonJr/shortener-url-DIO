2ad410c818d5e872eee20c95fa51e64e3e2c67de
KlaytonJr <klaytonjunior@protonmail.com>
2021-11-19 15:44:28 -0300

    Informations: 	- Full project

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://Cluser0:<password>@cluster0.cdn3i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
